fffa81f2d12660f1cb31af8c937a27ce23158de4
yossilev <yossi.lev.home@gmail.com>
2021-06-25 17:28:51 +0300

    header

---
+++ js/main.js
@@ -0,0 +1,33 @@
+addHeader = () => {
+    const header = document.createElement("div");
+    header.innerHTML = '<a href="/index.html">Home</a> - The site of new ideas';
+    header.classList.add('header');
+
+    const body = document.getElementsByTagName("BODY")[0];
+    body.insertBefore(header, body.firstChild);
+}
+optionalOpenClose = () => {
+    let vs = document.getElementsByClassName("optionalCloser")
+    for (let item in vs) {
+        console.log(vs[item]);
+        if (vs[item].style) {
+            vs[item].style.color = "red";
+        }
+        vs[item].onclick = (e) => {
+            let par = e.target.closest(".optionalParagraph");
+            par.classList.add("optionalStatusClosed");
+        }
+    }
+    vs = document.getElementsByClassName("optionalClosing")
+    for (let item in vs) {
+        console.log(vs[item]);
+        vs[item].onclick = (e) => {
+            let par = e.target.closest(".optionalParagraph");
+            par.classList.remove("optionalStatusClosed");
+        }
+    }
+}
+window.onload = () => {
+    addHeader();
+    optionalOpenClose();
+}
