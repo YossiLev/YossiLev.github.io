2f4a81c9661ec5f86760bab2edc4541e155e4370
yossilev <yossi.lev.home@gmail.com>
2021-07-09 15:21:21 +0300

    many things

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 addHeader = () => {
     const header = document.createElement("div");
-    header.innerHTML = '<a href="/index.html">Home</a> - The site of new ideas';
+    header.innerHTML = '<a style="color: white; float:left;" href="/index.html"><i onclick="" class="material-icons">home</i></a> The site of new ideas';
     header.classList.add('header');
 
     const body = document.getElementsByTagName("BODY")[0];
@@ -9,21 +9,35 @@ addHeader = () => {
 optionalOpenClose = () => {
     let vs = document.getElementsByClassName("optionalCloser")
     for (let item in vs) {
-        vs[item].onclick = (e) => {
-            let par = e.target.closest(".optionalParagraph");
-            par.classList.add("optionalStatusClosed");
+        if (vs.hasOwnProperty(item)) {
+            vs[item].onclick = (e) => {
+                let par = e.target.closest(".optionalParagraph");
+                par.classList.add("optionalStatusClosed");
+            }
         }
     }
     vs = document.getElementsByClassName("optionalClosing")
     for (let item in vs) {
-        console.log(vs[item]);
-        vs[item].onclick = (e) => {
-            let par = e.target.closest(".optionalParagraph");
-            par.classList.remove("optionalStatusClosed");
+        if (vs.hasOwnProperty(item)) {
+            vs[item].onclick = (e) => {
+                let par = e.target.closest(".optionalParagraph");
+                par.classList.remove("optionalStatusClosed");
+            }
+        }
+    }
+}
+zoomOnImages = () => {
+    let vs = document.getElementsByClassName("normalImage")
+    for (let item in vs) {
+        if (vs.hasOwnProperty(item)) {
+            vs[item].onclick = (e) => {
+                e.target.classList.toggle("extendedImage");
+            }
         }
     }
 }
 window.onload = () => {
     addHeader();
     optionalOpenClose();
+    zoomOnImages();
 }
