cb5d1d42e8bb4fcab53185513aebf6be7a56e91c
yossilev <yossi.lev.home@gmail.com>
2021-07-12 17:33:38 +0300

    content list

--- js/main.js
+++ js/main.js
@@ -10,47 +10,68 @@ addMasthead = () => {
     const body = document.getElementsByTagName("BODY")[0];
     body.insertBefore(header, body.firstChild);
 }
-    const pages = [
-        {display: "Abstract", page: "abstract"},
-        {display: "The Postulates", page: "postulates"},
-        {display: "Particles I", page: "particles1"},
-        {display: "Special Relativity *", page: "srMechanics"},
-        {display: "Gravitation", page: "gr"},
-        {display: "Cosmology", page: "cosmology"},
-        {display: "Particles II", page: "particles2"},
-        {display: "Electric Charge", page: "charge"},
-        {display: "Processes", page: "processes"},
-        {display: "Particles III", page: "particles3"},
-        {display: "Scattering", page: "scattering"},
-        {display: "QFT", page: "qft"},
-        {display: "Topology", page: "topology"},
-        {display: "Topological QFT", page: "tqft"},
-        {display: "Predictions", page: "predictions"},
-        {display: "Quantum Mechanics", page: "qm"},
-        {display: "Constants of Nature", page: "constants"},
-        {display: "Light", page: "light"},
-        {display: "Definitions", page: "definitions"},
-        {display: "Inventions", page: "inventions"},
-    ];
-    const goToPage = (pageIndex) => {
-        console.log(`go to ${pages[pageIndex].display}`);
-        const thisPage = window.location.href.match(/\/([a-zA-Z0-9]+)\.html/);
-        window.location.href = window.location.href.replace(`/${thisPage[1]}.html`, `/${pages[pageIndex].page}.html`)
+const pages = [
+    {display: "Abstract", page: "abstract"},
+    {display: "The Postulates", page: "postulates"},
+    {display: "Particles I", page: "particles1"},
+    {display: "Special Relativity *", page: "srMechanics"},
+    {display: "Gravitation", page: "gr"},
+    {display: "Cosmology", page: "cosmology"},
+    {display: "Particles II", page: "particles2"},
+    {display: "Electric Charge", page: "charge"},
+    {display: "Processes", page: "processes"},
+    {display: "Particles III", page: "particles3"},
+    {display: "Scattering", page: "scattering"},
+    {display: "QFT", page: "qft"},
+    {display: "Topology", page: "topology"},
+    {display: "Topological QFT", page: "tqft"},
+    {display: "Predictions", page: "predictions"},
+    {display: "Quantum Mechanics", page: "qm"},
+    {display: "Constants of Nature", page: "constants"},
+    {display: "Light", page: "light"},
+    {display: "Definitions", page: "definitions"},
+    {display: "Inventions", page: "inventions"},
+];
+const goToPage = (pageIndex) => {
+    console.log(`go to ${pages[pageIndex].display}`);
+    const thisPage = window.location.href.match(/\/([a-zA-Z0-9]+)\.html/);
+    window.location.href = window.location.href.replace(`/${thisPage[1]}.html`, `/${pages[pageIndex].page}.html`)
+}
+const removeContent = () => {
+    let vs = document.getElementsByClassName("contentList");
+    for (let item in vs) {
+        if (vs.hasOwnProperty(item)) {
+            vs[item].parentElement.removeChild(vs[item]);
+        }
     }
+}
+
+const displayContent = (pageIndex) => {
+    const content = document.createElement("div");
+    content.innerHTML = pages.map((p, ip) => {
+        return ip === pageIndex
+            ? `<div style="color: yellow">${ip + 1}. ${p.display}</div>`
+            : `<div style="cursor: pointer;" onclick="goToPage(${ip});">${ip + 1}. ${p.display}</div>`
+    }).join('');
+    content.classList.add('contentList');
+    content.onclick = (e) => {
+        removeContent();
+    }
+    const body = document.getElementsByTagName("BODY")[0];
+    body.insertBefore(content, body.firstChild);
+}
 addPager = () => {
     if (window.location.href.includes('/Theory')) {
         const thisPage = window.location.href.match(/\/([a-zA-Z0-9]+)\.html/);
         const index = pages.findIndex(p => p.page === thisPage[1]);
-        console.log(thisPage)
-        console.log(index)
         const header = document.createElement("div");
         header.innerHTML =
+            `<span onclick="displayContent(${index})" style="cursor: pointer;"><i class="material-icons" style="vertical-align: bottom;">list</i>Content list</span>` +
             (index > 0
-                ? `<span onclick="goToPage(${index - 1})" style="cursor: pointer; float:left;"><i class="material-icons">navigate_before</i>${pages[index - 1].display}</span>`
+                ? `<span onclick="goToPage(${index - 1})" style="cursor: pointer; float:left;"><i class="material-icons" style="vertical-align: bottom;">navigate_before</i>${pages[index - 1].display}</span>`
                 : '') +
-            `<span onclick="displayContent()" style="cursor: pointer; float:center"><i class="material-icons">list</i>Content list</span>` +
             (index < pages.length - 1
-                ? `<span onclick="goToPage(${index + 1})" style="cursor: pointer; float:right;">${pages[index + 1].display}<i class="material-icons">navigate_next</i></span>`
+                ? `<span onclick="goToPage(${index + 1})" style="cursor: pointer; float:right;">${pages[index + 1].display}<i class="material-icons" style="vertical-align: bottom;">navigate_next</i></span>`
                 : '');
         header.classList.add('pager');
 
